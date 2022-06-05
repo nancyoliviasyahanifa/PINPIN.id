c6512e6263a2432c6cb130865ea6906870b2cdbe
nancyoliviasyahanifa <nancyoliviasyahanifa@student.telkomuniversity.ac.id>
2022-06-05 22:17:49 +0700

    baru4

--- main.js
+++ main.js
@@ -5,6 +5,8 @@ setInterval(function () {
       console.log(res);
       document.getElementById("pintu").innerHTML = res.PINTU;
       document.getElementById("nama").innerHTML = res.NAMA;
-      document.getElementById("uid").innerHTML = res.UID;
+      document.getElementById("uid").innerHTML = res.UID_KTM;
+      document.getElementById("uid").innerHTML = res.UID_KTP;
+      document.getElementById("uid").innerHTML = res.UID_WHITECARD;
     });
 }, 1000);
